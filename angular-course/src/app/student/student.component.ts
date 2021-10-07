f0bbdb3887e30145c1de52068d56d1a4795f6436
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-10-07 10:45:46 +0200

    step 03 - ngServe styling

--- angular-course/src/app/student/student.component.ts
+++ angular-course/src/app/student/student.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-
-  constructor() { }
+studentList=[
+  {name: 'Ettore Esposito', hobby: 'Karate', gender: 'M'},
+  {name: 'Paolo Racca', hobby: 'Programmazione', gender: 'M'},
+  {name: 'Luca Dellavalle', hobby: 'Poligono', gender: 'M'},
+  {name: 'Bianca Teleman', hobby: 'Cheerleading', gender: 'F'},
+  {name: 'Martina Verlardi', hobby: 'Paddle', gender: 'F'},
+  {name: 'Andrea Ardizzone', hobby: 'Gaming', gender: 'M'},
+  {name: 'Marta Baudracco', hobby: 'Pesca', gender: 'F'},
+  {name: 'Michele Ghisolfi', hobby: 'Paddle', gender: 'M'},
+  {name: 'Giada Valinotto', hobby: 'Skincare', gender: 'F'},
+  {name: 'Gabriele Leone', hobby: 'Palestra', gender: 'M'},
+];
+student : any = {};
+  constructor() {
+    let num = Math.floor(Math.random()*this.studentList.length);
+    this.student= this.studentList[num];
+  }
 
   ngOnInit(): void {
   }
