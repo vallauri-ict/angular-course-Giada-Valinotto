1a33f16b4e9c793f05b86a237eee4e54d90f634e
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-10-15 11:41:14 +0200

    Bug fixing

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 
@@ -10,7 +11,8 @@ import { StudentComponent } from './student/student.component';
     StudentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
