ce965c2f46997ec767f97ab3ea3ec35c921c6b48
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-10-15 12:05:37 +0200

    Restarting project

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StudentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
